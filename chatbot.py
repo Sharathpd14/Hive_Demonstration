fec05a2716a418ff20b09acdf46b15452cedec06
Sharathpd14 <sharathpd14@gmail.com>
2025-03-13 20:36:56 +0530

    deployment ready

--- chatbot.py
+++ chatbot.py
@@ -3,33 +3,27 @@ from pydantic import BaseModel
 from groq import Groq
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 app = FastAPI()
 
-
-
 class Query(BaseModel):
     question: str
-    
+
 client = Groq(
     api_key=os.environ.get("GROQ_API_KEY"),
 )
 
-@app.post("/ask")
-async def ask_chatbot(query: Query):
+@app.post("/ask")  
+@app.get("/ask")   
+async def ask_chatbot(query: Query = None):
+    if not query:
+        return {"message": "Send a question using POST request."}
+
     chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content":query.question,
-        }
-    ],
-    model="gemma2-9b-it",
-)
+        messages=[{"role": "user", "content": query.question}],
+        model="gemma2-9b-it",
+    )
 
-    
     return {"response": chat_completion.choices[0].message.content}
-
-
-
